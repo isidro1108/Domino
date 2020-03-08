95b9f87c4fbca9fd7cb83188815629b857fb91e9
Isidro Morla <euris110811@gmail.com>
2020-03-08 15:08:41 -0400

    The sorted_tokens method has been renamed

--- table.py
+++ table.py
@@ -10,7 +10,7 @@ class Table:
             for v2 in range(v1, 7):
                 self.tokens.append(Token(v1, v2))
 
-    def sorted_tokens(self):
+    def shuffle_tokens(self):
         for n in range(len(self.tokens)):
             r = randint(0, len(self.tokens)-1)
             self.tokens[n], self.tokens[r] = self.tokens[r], self.tokens[n]
