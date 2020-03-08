f84555b7195f7783ad3c82b7adfbd055270e599b
Isidro Morla <euris110811@gmail.com>
2020-03-08 13:43:55 -0400

    There is a created_tokens method in the class Table and a tokens attribute in Player

--- table.py
+++ table.py
@@ -3,3 +3,8 @@ from token import Token
 class Table:
     def __init__(self):
         self.tokens = []
+
+    def create_tokens(self):
+        for v1 in range(7):
+            for v2 in range(v1, 7):
+                self.tokens.append(Token(v1, v2))
