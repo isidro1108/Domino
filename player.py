b5d9b66af487a072b0ff62adb6e8137d82501aec
Isidro Morla <euris110811@gmail.com>
2020-03-08 15:03:20 -0400

    The take_tokens method has been created in Player

--- player.py
+++ player.py
@@ -2,3 +2,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.tokens = []
+
+    def take_tokens(self, table):
+        for n in range(7):
+            self.tokens.append(table.tokens.pop())
