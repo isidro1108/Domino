87e54ec395e74d748fe5bc44bb14836bd41dfb82
Isidro Morla <euris110811@gmail.com>
2020-03-10 11:26:09 -0500

    The set_player and draw_table methods has been modified

--- game.py
+++ game.py
@@ -26,9 +26,15 @@ class Game:
         n_players = input('\nHow many players will have in the game?\n')
         if n_players not in '234' or len(n_players) != 1:
             return self.set_players()
-        for n in range(int(n_players)):
+        while len(self.players) != int(n_players):
+            if self.table.message != '':
+                print(self.table.message)
             name = input('Insert the name of the player: ')
-            self.players.append(Player(name))
+            self.table.message = ''
+            if len(name) > 1 and len(name) < 7:
+                self.players.append(Player(name))
+            else:
+                self.table.message = "The player's name must have more than 1 character and less than 7\n"
     
     def set_game(self):
         table = self.table
@@ -44,27 +50,31 @@ class Game:
         tokens_in_table = ''
         for token in self.table.tokens:
             tokens_in_table+= '[{}{}]'.format(token.values[0], token.values[1])
+        p_down = self.players[0].name
+        p_top = self.players[1].name if len(self.players) == 2 else self.players[2].name
+        p_right = '' if len(self.players) == 2 else 2 * ' ' + self.players[1].name
+        p_left = 6 * ' ' if len(self.players) < 4 else self.players[3].name + (6 - len(self.players[3].name)) * ' '
+
         fill = 6 * ' ' + '|' + 112 * ' ' + '|'
         fill_for_tokens = (56 - (len(tokens_in_table)//2)) * ' '
         table = [7 * ' ' + 112 * '_', fill, fill, 
-                6 * ' ' + '|' + fill_for_tokens + tokens_in_table + fill_for_tokens + '|',
-                fill, fill, 7 * ' ' + 112 * chr(175)]
+                p_left + '|' + fill_for_tokens + tokens_in_table + fill_for_tokens + '|' + p_right,
+                fill, fill, 7 * ' ' + 112 * chr(175) + '\n']
+        print((60 - len(p_top)//2) * ' ' + p_top)
         for line in table:
             print(line)
+        tokens = ''
+        for token in self.players[0].tokens:
+            tokens+= '[{}{}] '.format(token.values[0], token.values[1])
+        tokens_aside = ''
+        for token in self.table.tokens_aside:
+            tokens_aside+= '[||] '
+        print((60 - len(p_down)//2) * ' ' + p_down)
+        print((60 - len(tokens)//2) * ' ' + tokens)
+        print('\nTokens aside: ' + tokens_aside)
 
 game = Game()
+game.display_welcome()
+game.set_players()
+game.set_game()
 game.draw_table()
-# game.display_welcome()
-# game.set_players()
-# game.set_game()
-# for player in game.players:
-#     tokens = ''
-#     for token in player.tokens:
-#         tokens+= '[{}|{}] '.format(token.values[0], token.values[1])
-#     print(player.name, tokens)
-#     print()
-# tokens_aside = ''
-# for token in game.table.tokens_aside:
-#     tokens_aside+= '[{}|{}] '.format(token.values[0], token.values[1])
-# print('Tokens aside: ' + tokens_aside)
-# print(game.table.tokens)
