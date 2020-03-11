c5c21e1a3d49358a2e4b68bf42cdf4841a9d9308
Isidro Morla <euris110811@gmail.com>
2020-03-10 21:35:43 -0400

    The table is more big and the draw_welcome method has been added in Game

--- game.py
+++ game.py
@@ -8,16 +8,19 @@ class Game:
         self.table = Table()
         self.players = []
 
+    def draw_welcome(self):
+        print('\n\n' + 71 * '*' + 11 * ' ' + 71 * '*')
+        print(71 * '*' + 'Domino Game' + 71 * '*')
+        print(71 * '*' + 11 * ' ' + 71 * '*' + '\n')
+
     def display_welcome(self):
         bar = 15 * ' '
         for n in range(15):
             system('cls')
             bar = bar[:n] + '|' + bar[n + 1:]
-            print('\n\n' + 20 * '*' + 11 * ' ' + 20 * '*')
-            print(20 * '*' + 'Domino Game' + 20 * '*')
-            print(20 * '*' + 11 * ' ' + 20 * '*' + '\n')
-            print(17 * ' ' + '[' + bar + ']')
-            print(24 * ' ' + '{}%'.format(round(n/15 * 100)))
+            self.draw_welcome()
+            print(68 * ' ' + '[' + bar + ']')
+            print(75 * ' ' + '{}%'.format(round(n/15 * 100)))
             sleep(random())
 
     def set_players(self):
@@ -49,28 +52,29 @@ class Game:
         system('cls')
         tokens_in_table = ''
         for token in self.table.tokens:
-            tokens_in_table+= '[{}{}]'.format(token.values[0], token.values[1])
+            tokens_in_table+= '[{}|{}]'.format(token.values[0], token.values[1])
         p_down = self.players[0].name
         p_top = self.players[1].name if len(self.players) == 2 else self.players[2].name
-        p_right = '' if len(self.players) == 2 else 2 * ' ' + self.players[1].name
+        p_right = '' if len(self.players) == 2 else (6 - len(self.players[1].name)) * ' ' + self.players[1].name
         p_left = 6 * ' ' if len(self.players) < 4 else self.players[3].name + (6 - len(self.players[3].name)) * ' '
 
-        fill = 6 * ' ' + '|' + 112 * ' ' + '|'
-        fill_for_tokens = (56 - (len(tokens_in_table)//2)) * ' '
-        table = [7 * ' ' + 112 * '_', fill, fill, 
+        fill = 6 * ' ' + '|' + 140 * ' ' + '|'
+        fill_for_tokens = (70 - (len(tokens_in_table)//2)) * ' '
+        table = [7 * ' ' + 140 * '_', fill, fill, 
                 p_left + '|' + fill_for_tokens + tokens_in_table + fill_for_tokens + '|' + p_right,
-                fill, fill, 7 * ' ' + 112 * chr(175) + '\n']
-        print((60 - len(p_top)//2) * ' ' + p_top)
+                fill, fill, 7 * ' ' + 140 * chr(175) + '\n']
+        self.draw_welcome()
+        print((74 - len(p_top)//2) * ' ' + p_top)
         for line in table:
             print(line)
         tokens = ''
         for token in self.players[0].tokens:
-            tokens+= '[{}{}] '.format(token.values[0], token.values[1])
+            tokens+= '[{}|{}] '.format(token.values[0], token.values[1])
         tokens_aside = ''
         for token in self.table.tokens_aside:
-            tokens_aside+= '[||] '
-        print((60 - len(p_down)//2) * ' ' + p_down)
-        print((60 - len(tokens)//2) * ' ' + tokens)
+            tokens_aside+= '[|||] '
+        print((74 - len(p_down)//2) * ' ' + p_down)
+        print((74 - len(tokens)//2) * ' ' + tokens)
         print('\nTokens aside: ' + tokens_aside)
 
 game = Game()
